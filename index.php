c69654cb91cf7f3b8248d207ac538e9cb67abba8
v1ctor13 <victorviniciuspj@gmail.com>
2022-09-08 14:04:35 -0300

    javascript removed

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
         <?php
             imprimirTabela($arr);
             $coord = explode(",", $arr['loc']);
-            
+
             echo "<img src=\"https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/". $arr['loc'] ."/12?mapSize=800,800&mapLayer=Basemap,Buildings&key=AlLKUFpKxYkRLKHj1HgaIxdlmapv50uc4cGjXE-6uLfF_jx7nwkuksGDtg-O2_iU\">"
         ?>
     </div>
@@ -48,10 +48,5 @@
             <a href="http://v1ctor.epizy.com"><img src="img/portfolio.png" target="_blank"></a> 
         </div>
     </footer>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
